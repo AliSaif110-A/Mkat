10ac20965334e7fc47d61e935791e5066511fcf7
Ali <enggalisaif.110@gmail.com>
2023-04-08 15:22:11 +0100

    make it better

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+
+app.use(express.static('public'));
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname + "/index.html"));
+})
+
+
+app.get("/contacts.html", (req, res) => {
+    res.sendFile(path.join(__dirname + "/payment.html"));
+})
+// serving the index.html file 
+
+const server = app.listen(5000);
+const portNumber = server.address().port;
+console.log(`port: ${portNumber}`);
+// can see the port number in terminal - you can dictate the port number

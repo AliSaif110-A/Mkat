68a435590102d2c8cc18b33582fbd380d4ac35ae
Ali <enggalisaif.110@gmail.com>
2023-04-08 15:29:43 +0100

    make it better

--- server.js
+++ server.js
@@ -15,7 +15,8 @@ app.get("/contacts.html", (req, res) => {
 })
 // serving the index.html file 
 
-const server = app.listen(5000);
+var PORT = process.env.PORT || 5000;
+const server = app.listen(PORT);
 const portNumber = server.address().port;
 console.log(`port: ${portNumber}`);
 // can see the port number in terminal - you can dictate the port number
